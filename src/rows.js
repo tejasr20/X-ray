27a31e11da08b994143507e163d50f3e9df14348
Tejas Ramakrishnan <82379532+tejasr20@users.noreply.github.com>
2022-03-11 10:37:32 +0530

    Initial commit

---
+++ src/rows.js
@@ -0,0 +1,15 @@
+const rows = [
+  {
+    UniqueID: "1234",
+    Date: "08/03/2022",
+    DoctorName: "Dr. Gauranga ",
+    Prescribed_X_Rays: "Right ankle"
+  },
+  {
+    UniqueID: "1235",
+    Date: "01/03/2022",
+    DoctorName: "Dr. Krishna Yadav",
+    Prescribed_X_Rays: "Stomach"
+  }
+];
+export default rows;
